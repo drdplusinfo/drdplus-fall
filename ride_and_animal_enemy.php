f50faba282991a8d4854ff94d7fabd4f3e8456da
Jaroslav Týc <mail@jaroslavtyc.com>
2017-07-04 10:24:50 +0200

    Preparation for riding animals with height

--- ride_and_animal_enemy.php
+++ ride_and_animal_enemy.php
@@ -3,6 +3,8 @@ namespace DrdPlus\Fight;
 
 use DrdPlus\Codes\Skills\PhysicalSkillCode;
 use DrdPlus\Codes\Skills\PsychicalSkillCode;
+use DrdPlus\Codes\Transport\RidingAnimalCode;
+use DrdPlus\Tables\Measurements\Distance\Distance;
 
 /** @var Controller $controller */
 ?>
@@ -12,50 +14,19 @@ use DrdPlus\Codes\Skills\PsychicalSkillCode;
             <input type="checkbox" value="1"
                    name="<?= $controller::ON_HORSEBACK ?>"
                    <?php if ($controller->getSelectedOnHorseback()) { ?>checked="checked" <?php } ?>>
-            Bojuješ ze sedla
+            Padáš z "koně"
         </label>
-    </div>
-    <div class="block">
-        Dovednost <span
-                class="keyword"><?= PhysicalSkillCode::getIt(PhysicalSkillCode::RIDING)->translateTo('cs') ?></span>
-        <label>na stupni <input type="radio" value="0" name="<?= $controller::RIDING_SKILL_RANK ?>"
-                                <?php if ($controller->getSelectedRidingSkillRank() === 0) { ?>checked<?php } ?>>
-            0,
-        </label>
-        <label><input type="radio" value="1" name="<?= $controller::RIDING_SKILL_RANK ?>"
-                      <?php if ($controller->getSelectedRidingSkillRank() === 1) { ?>checked<?php } ?>> 1,
-        </label>
-        <label><input type="radio" value="2" name="<?= $controller::RIDING_SKILL_RANK ?>"
-                      <?php if ($controller->getSelectedRidingSkillRank() === 2) { ?>checked<?php } ?>> 2,
-        </label>
-        <label><input type="radio" value="3" name="<?= $controller::RIDING_SKILL_RANK ?>"
-                      <?php if ($controller->getSelectedRidingSkillRank() === 3) { ?>checked<?php } ?>> 3
-        </label>
-    </div>
-    <div class="block"><input type="submit" value="Přepočítat"></div>
-</div>
-<div class="panel">
-    <div class="block">
-        <label><input type="checkbox" value="1"
-                      name="<?= $controller::FIGHT_FREE_WILL_ANIMAL ?>"
-                      <?php if ($controller->getSelectedFightFreeWillAnimal()) { ?>checked="checked" <?php } ?>>
-            Bojuješ se zvířetem s vlastní vůlí</label>
-    </div>
-    <div class="block">
-        Dovednost <span
-                class="keyword"><?= PsychicalSkillCode::getIt(PsychicalSkillCode::ZOOLOGY)->translateTo('cs') ?></span>
-        <label>na stupni <input type="radio" value="0" name="<?= $controller::ZOOLOGY_SKILL_RANK ?>"
-                                <?php if ($controller->getSelectedZoologySkillRank() === 0) { ?>checked<?php } ?>>
-            0,
-        </label>
-        <label><input type="radio" value="1" name="<?= $controller::ZOOLOGY_SKILL_RANK ?>"
-                      <?php if ($controller->getSelectedZoologySkillRank() === 1) { ?>checked<?php } ?>> 1,
-        </label>
-        <label><input type="radio" value="2" name="<?= $controller::ZOOLOGY_SKILL_RANK ?>"
-                      <?php if ($controller->getSelectedZoologySkillRank() === 2) { ?>checked<?php } ?>> 2,
-        </label>
-        <label><input type="radio" value="3" name="<?= $controller::ZOOLOGY_SKILL_RANK ?>"
-                      <?php if ($controller->getSelectedZoologySkillRank() === 3) { ?>checked<?php } ?>> 3
+        <label>
+            "Kůň"
+            <select name="riding-animal">
+                <?php /**
+                 * @var float $heightInMeters
+                 * @var RidingAnimalCode $ridingAnimal
+                 */
+                foreach ($controller->getRidingAnimalsWithHeight() as $heightInMeters => $ridingAnimal) { ?>
+                    <option value="<?= $ridingAnimal ?>"><?= "{$ridingAnimal} ({$heightInMeters} m)" ?></option>
+                <?php } ?>
+            </select>
         </label>
     </div>
     <div class="block"><input type="submit" value="Přepočítat"></div>
